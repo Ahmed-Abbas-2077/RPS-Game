aa782bc8ae929d18dcd5556606d7f135246200aa
Ahmed Abbas <111700915+Ahmed-Abbas-2077@users.noreply.github.com>
2023-08-30 02:55:35 +0300

    playRound to play the game

--- script.js
+++ script.js
@@ -5,11 +5,32 @@ function computerPlay() {
   return rps[random];
 }
 
-console.log(computerPlay());
-
 function playerPlay() {
   let playerSelection = prompt('Rock, Paper, or Scissors?');
   return playerSelection.toLowerCase();
 }
 
-console.log(playerPlay());
+function playRound(playerPlay, computerPlay) {
+  let player = playerPlay;
+  let computer = computerPlay;
+
+  if (player === computer) {
+    return "It's a tie!";
+  } else if (player === 'rock' && computer === 'scissors') {
+    return 'You win! Rock beats scissors.';
+  } else if (player === 'paper' && computer === 'rock') {
+    return 'You win! Paper beats rock.';
+  } else if (player === 'scissors' && computer === 'paper') {
+    return 'You win! Scissors beats paper.';
+  } else if (player === 'rock' && computer === 'paper') {
+    return 'You lose! Paper beats rock.';
+  } else if (player === 'paper' && computer === 'scissors') {
+    return 'You lose! Scissors beats paper.';
+  } else if (player === 'scissors' && computer === 'rock') {
+    return 'You lose! Rock beats scissors.';
+  } else {
+    return 'Invalid input.';
+  }
+}
+
+console.log(playRound(playerPlay(), computerPlay()));
